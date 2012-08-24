db7371fa6455819441b31093a0097d0c64316f22
Pedro Garcia <pedro@sshsmedia.com>
2012-08-24 11:09:27 -0400

    init

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module Carro
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.assets.precompile += ['style.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
